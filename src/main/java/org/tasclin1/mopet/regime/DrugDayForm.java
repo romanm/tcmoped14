870e5a3b78a608b90ec75d59445aa1c726838e1d
roman <roman.mishchenko@gmail.com>
2012-10-14 20:22:26 +0200

    drug day form v2

--- src/main/java/org/tasclin1/mopet/regime/DrugDayForm.java
+++ src/main/java/org/tasclin1/mopet/regime/DrugDayForm.java
@@ -7,7 +7,7 @@ import java.util.Set;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
-public class DayPeriod implements Serializable {
+public class DrugDayForm implements Serializable {
     private static final long serialVersionUID = 1L;
     protected final Log log = LogFactory.getLog(getClass());
 
--- src/main/java/org/tasclin1/mopet/service/RegimeDrugService.java
+++ src/main/java/org/tasclin1/mopet/service/RegimeDrugService.java
@@ -4,19 +4,15 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
-import org.tasclin1.mopet.regime.DayPeriod;
+import org.tasclin1.mopet.regime.DrugDayForm;
 
 @Service("regimedrugService")
 @Repository
 public class RegimeDrugService {
     protected final Log log = LogFactory.getLog(getClass());
 
-    public DayPeriod getDayPeriod() {
-	return new DayPeriod();
+    public DrugDayForm getDrugDayForm() {
+	return new DrugDayForm();
     }
 
-    public void dayPeriodOnEntry(DayPeriod dayPeriod) {
-	log.debug("-------------------");
-	dayPeriod.initPeriod();
-    }
 }
